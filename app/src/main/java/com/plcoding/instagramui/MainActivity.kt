c6bff5bea59f23bfb3ec169a0f248c1d2569dea4
Erfan <110898549+Erfanandroid@users.noreply.github.com>
2023-03-23 18:56:27 +0430

    The final Commit

--- app/src/main/java/com/plcoding/instagramui/MainActivity.kt
+++ app/src/main/java/com/plcoding/instagramui/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ProfileScreen()
+            println("Hello Instagram")
         }
     }
 }
